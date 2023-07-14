fa04b15ba93506c2cf97a27ad0acc8497da18eb8
kannan112 <kannangt100@gmail.com>
2023-07-15 02:07:03 +0530

    user table test

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -1,7 +1,8 @@
 package domain
 
 type Users struct {
-	ID      uint   `json:"id" gorm:"unique;not null"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
+	ID       uint   `json:"id" gorm:"unique;not null"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
